dfd61e6074536c1d394bdfe7d8be5153c4ae08cc
Kanwarbir02 <kanwarbir2002@gmail.com>
2021-08-19 23:24:55 +0530

    Loading state

--- src/Homepage.js
+++ src/Homepage.js
@@ -2,7 +2,8 @@ import {useState, useEffect} from "react";
 import BlogList from "./blog_list";
 
 const Homepage = () => {
-    const [blogs, setBlogs] = useState(null)
+    const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true);
 
     const handleDelete = (deleteId) => {
         // This line of code makes a new array of blogs, filering out the ones not to be deleted
@@ -20,12 +21,14 @@ const Homepage = () => {
           })
           .then(data => {
            setBlogs(data);
+           setIsPending(false)
           })
       }, [])
 
     return ( 
         
         <div className="home">
+            {isPending && <h2>Loading blogs...</h2>}
            {blogs && <BlogList blogs={blogs} title="All Blogs" handleDelete = {handleDelete}/> } 
             
         {/* <BlogList blogProp={blogs.filter((blog) => blog.author.toLowerCase() === "jake")} title="Jake's Blogs" /> */}
