722aa576a8e0676c11390aeb46e0e1078b066571
Kanwarbir02 <kanwarbir2002@gmail.com>
2021-08-19 14:07:42 +0530

    Reuse components for separate titles

--- src/Homepage.js
+++ src/Homepage.js
@@ -3,15 +3,18 @@ import BlogList from "./blog_list";
 
 const Homepage = () => {
     const [blogs, setBlogs] = useState([
-        {author:"Jake", content: "Lorem Ipsum is simply dummy text", id:1},
-        {author:"Shelly", content: "Lorem Ipsum is simply  text", id:2},
-        {author:"Robert", content: "Lorem Ipsum is simply dummy text", id:3}
+        {author:"Jake", content: "Lorem Ipsum is simply dummy text", title: "Art Of Jake", id:1},
+        {author:"Shelly", content: "Lorem Ipsum is simply  text", title: "Art Of Shelly", id:2},
+        {author:"Robert", content: "Lorem Ipsum is simply dummy text", title: "Art Of Robert", id:3},
+        {author:"Jake", content: "Lorem Ipsum ....", title: "Art Of Jake", id:4}
     ])
     
     return ( 
         
         <div className="home">
-            <BlogList blogProp={blogs}/>
+            <BlogList blogProp={blogs} title="All Blogs"/>
+
+            <BlogList blogProp={blogs.filter((blog) => blog.author.toLowerCase() === "jake")} title="Jake's Blogs" />
             
         </div>
      );
--- src/blog_list.js
+++ src/blog_list.js
@@ -1,11 +1,14 @@
 const BlogList = (props) => {
     const blogs = props.blogProp;
+    const title = props.title
 
 
     return ( 
         <div className="bloglist">
+            <h1>{title}</h1>
             {blogs.map((blog) => (
             <div className="blog-preview" key={blog.id}>
+                
                 <h2>{blog.content}</h2>
                 <p>Written By {blog.author}</p>
             </div>
