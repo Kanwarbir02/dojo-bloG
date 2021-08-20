3d6d76ce2d6c2a0eeaf63bcef927dd13329721da
Kanwarbir02 <kanwarbir2002@gmail.com>
2021-08-20 16:12:46 +0530

    Handle fetch errors

--- src/Homepage.js
+++ src/Homepage.js
@@ -4,6 +4,7 @@ import BlogList from "./blog_list";
 const Homepage = () => {
     const [blogs, setBlogs] = useState(null);
     const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
 
     const handleDelete = (deleteId) => {
         // This line of code makes a new array of blogs, filering out the ones not to be deleted
@@ -12,24 +13,31 @@ const Homepage = () => {
     }
 
 
-    const [name, setName] = useState("Mario");
-
     useEffect(() => {
         fetch('http://localhost:8000/blogs')
-          .then(res => {
-            return res.json();
-          })
-          .then(data => {
-           setBlogs(data);
-           setIsPending(false)
-          })
+            .then(res => {
+                if(res.ok !== true){
+                  throw Error("not able to fetch data from json...");
+                }
+                return res.json();
+            })
+            .then(data => {
+                setBlogs(data);
+                setIsPending(false)
+                setError(null)
+            })
+            .catch(err => {
+                setError(err.message);
+                setIsPending(false);
+            })
       }, [])
 
     return ( 
         
         <div className="home">
-            {isPending && <h2>Loading blogs...</h2>}
-           {blogs && <BlogList blogs={blogs} title="All Blogs" handleDelete = {handleDelete}/> } 
+          {error && <h2>not able to fetch data from json...</h2>}
+          {isPending && <h2>Loading blogs...</h2>}
+          {blogs && <BlogList blogs={blogs} title="All Blogs" handleDelete = {handleDelete}/> } 
             
         {/* <BlogList blogProp={blogs.filter((blog) => blog.author.toLowerCase() === "jake")} title="Jake's Blogs" /> */}
             
