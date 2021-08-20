bc27ab49458aebeef53a59c8776fab80c4ab4075
Kanwarbir02 <kanwarbir2002@gmail.com>
2021-08-21 02:52:39 +0530

    Reuse useFetch hook for singular blog post

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,11 +1,21 @@
 import { useParams } from "react-router-dom";
-
+import useFetch from "./useFetch";
 const BlogDetails = () => {
-
+    
     const {id} = useParams();
 
+    const {data: blog, isPending, error} = useFetch('http://localhost:8000/blogs/' + id);
+
     return ( 
-        <h2>Blog Details - {id}</h2>
+        <div>
+            {isPending && <p>Loading...</p>}
+            {error && <p>{error}</p>}
+            {blog && <div>
+                <h2>{blog.title}</h2>
+                <h4>Written by {blog.author}</h4>
+                <p>{blog.body}</p>
+            </div>}
+        </div>
      );
 }
  
