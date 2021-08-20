a62b8056fb1334b943d85675ef4a9fdfdba3d0de
Kanwarbir02 <kanwarbir2002@gmail.com>
2021-08-20 19:51:51 +0530

    react router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import Navbar from './Navbar';
 import Homepage from './Homepage';
 import './index.css';
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom"
 
 function App() {
   
 
   return (
-
-    <div className="App">
+    <Router>
+      <div className="App">
       < Navbar />
-      < Homepage/ >
+      <div className="content">
+        <Switch>
+          <Route path="/" >
+              <Homepage />
+          </Route>
+        </Switch>
+      </div>
       
     </div>
+    </Router>
+    
   );
 }
 
