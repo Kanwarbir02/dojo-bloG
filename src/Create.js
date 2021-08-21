2178c3bb02c9a502b4f3ba0b01e125b36dc5b3f4
Kanwarbir02 <kanwarbir2002@gmail.com>
2021-08-21 13:19:07 +0530

    Make POST requests

--- src/Create.js
+++ src/Create.js
@@ -2,16 +2,23 @@ import { useState } from "react";
 
 const Create = () => {
 
-    const [b_title, setTitle] = useState("");
-    const [b_body, setBody] = useState("");
-    const [b_author, setAuthor] = useState("mario");
+    const [title, setTitle] = useState("");
+    const [body, setBody] = useState("");
+    const [author, setAuthor] = useState("mario");
 
     const handleSubmit = e => {
         e.preventDefault();
-        const blog = {b_title, b_body, b_author};
+        const blog = {b_title: title, b_body: body, b_author: author};
 
-        console.log(blog)
-    }
+        fetch('http://localhost:8000/blogs/', {
+            method: 'POST',
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(blog)
+            }).then(() => {
+            console.log('new blog added');
+        })
+  }
+    
 
     return ( 
         <div className="create">
@@ -19,13 +26,13 @@ const Create = () => {
             <form onSubmit={handleSubmit}>
 
                 <label>Title</label>
-                <input type="text" required value={b_title} onChange={e => setTitle(e.target.value)} />
+                <input type="text" required value={title} onChange={e => setTitle(e.target.value)} />
 
                 <label>Body</label>
-                <textarea required value={b_body} onChange={e => setBody(e.target.value)}> </textarea>
+                <textarea required value={body} onChange={e => setBody(e.target.value)}> </textarea>
 
                 <label>Written by: </label>
-                <select value={b_author} onChange={e => setAuthor(e.target.value)}>
+                <select value={author} onChange={e => setAuthor(e.target.value)}>
                     <option value="mario">mario</option>
                     <option value="yoshi">yoshi</option>
                 </select>
