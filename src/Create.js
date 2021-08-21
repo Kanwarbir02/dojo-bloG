0486ea31d87abef601a85a83c2131d8aa1e89ff4
Kanwarbir02 <kanwarbir2002@gmail.com>
2021-08-21 12:07:14 +0530

    Create new form blog and save inputs

--- src/Create.js
+++ src/Create.js
@@ -1,8 +1,34 @@
+import { useState } from "react";
+
 const Create = () => {
+
+    const [b_title, setTitle] = useState("");
+    const [b_body, setBody] = useState("");
+    const [b_author, setAuthor] = useState("");
+
     return ( 
         <div className="create">
             <h2>Add a new blog</h2>
+            <form>
+                <label>Title</label>
+                <input type="text" required value={b_title} onChange={e => setTitle(e.target.value)} />
+
+                <label>Body</label>
+                <textarea required value={b_body} onChange={e => setBody(e.target.value)}> </textarea>
+
+                <label>Written by: </label>
+                <select value={b_author} onChange={e => setAuthor(e.target.value)}>
+                    <option value="mario">mario</option>
+                    <option value="yoshi">yoshi</option>
+                </select>
+
+                <button>Add Blog</button>
+            </form>
+
         </div>
+
+
+        
      );
 }
  
